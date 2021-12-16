99d53449e7ea69b1b13121e936ba8fca98923672
Secoldevs <elyordiusc@gmail.com>
2021-12-16 15:15:37 -0500

    Add vehicle class

---
+++ src/model/Vehicle.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Vehicle {
+    private String plate, brand, model, color;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String plate, String brand, String model, String color) {
+        this.plate = plate;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "plate='" + plate + '\'' +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
